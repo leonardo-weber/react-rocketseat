4cd8a31a10e55c0acc9ba59c729359c208e5d61e
leonardo-weber <weberleonardo2002@gmail.com>
2023-07-27 08:38:13 -0300

    advancing in project 02

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-    
-
-    oi
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+      
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
---
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from "./styles"
+import igniteLogo from '../../assets/ignite-logo.svg'
+import { Scroll, Timer } from "phosphor-react"
+import { NavLink } from "react-router-dom"
+
+export const Header = () => {
+    return (
+        <HeaderContainer>
+            <img src={igniteLogo} alt="" />
+            <nav>
+                <NavLink to="/" title="Timer"> 
+                    <Timer size={24} />
+                 </NavLink>
+                <NavLink to="/history" title="History"> 
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
+    )
+}
