4bc07c23d027247c8ff01497af00da82b98a9e91
leonardo-weber <weberleonardo2002@gmail.com>
2023-07-28 11:45:12 -0300

    home styles

---
+++ 02-ignite-timer/src/pages/History/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+
+    flex: 1
+
+`
+
+export const HistoryList = styled.div`
+
+
+`
--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,9 +1,52 @@
+import { Play } from "phosphor-react"
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, StartCountdownButton, TaskInput } from "./styles"
+
 export const Home = () => {
     return (
-        <div>
+        <HomeContainer>
             <form>
-                
+                <FormContainer>
+
+                    <label htmlFor="task"> Vou trabalhar em </label>
+                    <TaskInput 
+                        id="task" 
+                        type="text" 
+                        placeholder="De um nome para o seu projeto" 
+                        list="taskSuggestions"
+                    />
+
+                    <datalist id="taskSuggestions">
+                        <option value=""></option>
+                    </datalist>
+
+                    <label htmlFor="minutesAmount"> Durante </label>
+                    <MinutesAmountInput 
+                        id="minutesAmount" 
+                        type="number" 
+                        placeholder="00" 
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
+
+                    <span> minutos. </span>
+
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span> 0 </span>
+                    <span> 0</span>
+                    <span> : </span>
+                    <span> 0</span>
+                    <span> 0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton type="submit"> 
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
             </form>
-        </div>
+
+        </HomeContainer>
     )
 }
