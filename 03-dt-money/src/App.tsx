65d19cccaf67c153b927939abc2a05677f762a05
leonardo-weber <weberleonardo2002@gmail.com>
2023-08-15 11:58:36 -0300

    searchform and transactions

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Transactions } from "./pages/transactions";
 import { GlobalStyle } from './styles/global'
-import { Summary } from "./components/Summary";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
 
       <Transactions />
-      <Summary />
 
       <GlobalStyle />
       
--- 03-dt-money/src/pages/transactions/index.tsx
+++ 03-dt-money/src/pages/transactions/index.tsx
@@ -1,9 +1,38 @@
 import { Header } from "../../components/Header"
+import { SearchForm } from "../../components/SearchForm"
+import { Summary } from "../../components/Summary"
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export const Transactions = () => {
     return (
         <>
             <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width={'50%'}> Desenvolvimento de site </td>
+                            <PriceHighlight variant={'income'}>
+                                R$ 12.000,00
+                            </PriceHighlight>
+                            <td> Venda </td>
+                            <td> 13/04/2022 </td>
+                        </tr>
+                        <tr>
+                            <td width={'50%'}> Hamburguer </td>
+                            <PriceHighlight variant={'outcome'}>
+                                - R$ 58,00
+                            </PriceHighlight>
+                            <td> Alimentação </td>
+                            <td> 13/04/2022 </td>
+                        </tr>
+
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </>
     )
 }
